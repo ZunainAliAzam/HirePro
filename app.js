720d56361d655841afa428920561552e5ba6f021
ZunainAliAzam <zunainazam456@gmail.com>
2024-01-03 21:55:47 +0500

    commitChanges

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper(".js-testimonials-slider", {
-      grabCursor: true,
-      spaceBetween: 30,
-      pagination: {
-        el: ".js-testimonials-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        767: {
-          slidesPerView: 2,
+        grabCursor: true,
+        spaceBetween: 30,
+        pagination: {
+            el: ".js-testimonials-pagination",
+            clickable: true,
         },
-      },
+        breakpoints: {
+            767: {
+                slidesPerView: 2,
+            },
+        },
     });
-  });
 
-let menuBtn = document.querySelector("#menu-btn");
-let menu = document.querySelector(".navbar");
+    let menuBtn = document.querySelector("#menu-btn");
+    let menu = document.querySelector(".navbar");
 
-menuBtn.onclick = () => {
-    menu.classList.toggle('active');
-}
+    menuBtn.onclick = () => {
+        menu.classList.toggle('active');
+    }
+});
